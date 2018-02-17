5526988e7dcf89cec658d7206a2c3489c650db05
Thomas Casady <tscasady@gmail.com>
2018-02-17 04:15:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,65 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.random() * 100 + 1
+ itemPrice = Math.floor(itemPrice)
+ cart.push(new Object({[item]: itemPrice})) 
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.')
+  }
+    else{
+    var cartStatement = 'In your cart, you have ' 
+    var cartArray
+    for (var i=0; i < cart.length; i++){
+      if (cart.length-1 === 0){
+      cartArray = Object.keys(cart[i])
+      cartStatement = cartStatement + `${cartArray[0]} at $${cart[i][cartArray[0]]}.` 
+      console.log(cartStatement)
+    }
+      else if (i === cart.length - 1){
+      cartArray = Object.keys(cart[i])
+      cartStatement = cartStatement.slice(0,-2) + ` and ${cartArray[0]} at $${cart[i][cartArray[0]]}.`
+      console.log(cartStatement)
+     }    else{
+       cartArray = Object.keys(cart[i])
+       cartStatement = cartStatement +`${cartArray[0]} at $${cart[i][cartArray[0]]}, ` 
+    }
+  }
+  }
+  console.log(cartStatement)
 }
 
 function total() {
-  // write your code here
+  var totalValue = 0
+  for (var i = 0; i < cart.length; i++){
+    var cartArray = Object.keys(cart[i])
+    totalValue = totalValue + cart[i][cartArray[0]]
+  }
+  return totalValue
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var tempCartLength = cart.length
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item) === true){
+      var index = cart.indexOf(cart[i])
+      cart = cart.slice(index, 1)
+      return cart
+    }
+  }
+  if (tempCartLength === cart.length){
+    console.log("That item is not in your cart.")
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  
 }
